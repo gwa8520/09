6c0b9d6e81b1f692b313518daef0f473f84e79bd
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-11-03 19:19:45 +0900

    23.11.03 5

--- 09.c
+++ 09.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE  4
+#define ROWS  3
+#define COLS  3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-void squre_array(int a[],int size){
-	int i;
+void addMatrix(int A[][COLS], int B[][COLS]){
+	//C=A+B : for loop
+	int i,j;
+	int SUM[ROWS][COLS];
+		for(i=0; i<2; i++)
+			for(j=0;j<2;j++)
+				SUM[i][j]=A[ROWS][COLS]+B[ROWS][COLS];
+			
+		for(i=0; i<2; i++)
+			for(j=0;j<2;j++)
+				printf("C[%i][%i]=%2d",i,j,SUM[i][j]);
+			
+			printf("\n");
 	
-	for(i=0; i<SIZE; i++)
-		a[i]=a[i]*a[i];
 }
 
-void print_array(int a[], int size){
-	int i;
-	
-	for(i=0;i<SIZE;i++)
-		printf("%3d", a[i]);
-	printf("\n");
-}
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int list[SIZE]={1,2,3,4};
+	int A[ROWS][COLS]={
+		{2,3,0},{8,9,1},{7,0,5}};
 		
-	print_array(list,SIZE);
-	squre_array(list,SIZE);  //원본이 가기에 제곱된 값이 출력이 된다.  
-	print_array(list,SIZE);
+	int B[ROWS][COLS]={
+		{1,0,0},{0,1,0},{0,0,1}};
+		
+	int C[ROWS][COLS]={};
 	
+	
+	Matrix(A,B,C);
 	
 	return 0;
 }
